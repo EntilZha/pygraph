3efe9e831a41aa63e772aff784871295b66e05c1
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-02-16 19:05:00 -0700

    Prepare for pypi release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pygraph',
+    name='pygraph-cli',
     description='CLI for pygraphviz powered by graphviz',
     url='https://github.com/EntilZha/pygraph',
     author='Pedro Rodriguez',
